c61c077ea125dccbfa9e7d39f57c3250837853b1
A.R. Maged <abdulrahman.maged95@gmail.com>
2018-04-21 16:23:24 +0200

    :bug: Append transaction to buffer before broadcasting

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -33,8 +33,8 @@ public class User {
   void createTransaction() {
     Transaction newTransaction = new Transaction();
 
-    this.broadcastTransaction(newTransaction);
     this.transactionsBuffer.add(newTransaction);
+    this.broadcastTransaction(newTransaction);
 
     if (this.isReadyToMine()) {
       List<Transaction> groupedTransactions =
@@ -55,8 +55,8 @@ public class User {
       return;
     }
 
-    this.broadcastTransaction(transaction);
     this.transactionsBuffer.add(transaction);
+    this.broadcastTransaction(transaction);
 
     if (this.isReadyToMine()) {
       List<Transaction> groupedTransactions =
