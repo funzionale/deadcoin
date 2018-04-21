3571fa54792802f3cd9591220ba921a2cb9c3dba
A.R. Maged <abdulrahman.maged95@gmail.com>
2018-04-21 17:05:36 +0200

    :sparkles: Create uuid() & now() utility methods

--- src/main/java/Utils.java
+++ src/main/java/Utils.java
@@ -1,5 +1,7 @@
+import java.time.Instant;
 import java.util.List;
 import java.util.Random;
+import java.util.UUID;
 import java.util.function.BiConsumer;
 
 public class Utils {
@@ -9,6 +11,14 @@ public class Utils {
 
   public static int random(int lowerBound, int upperBound) {
     return new Random().nextInt(upperBound - lowerBound) + lowerBound;
+  }
+
+  public static String uuid() {
+    return UUID.randomUUID().toString();
+  }
+
+  public static long now() {
+    return Instant.now().getEpochSecond();
   }
 
   public static <T> void forEachIndexed(BiConsumer<? super T, Integer> consumer, List<T> list) {
